d06a68f376539a665ed80b008034e555a018cf37
Shay Stevens <stesh969@student.otago.ac.nz>
2022-03-11 11:56:54 +1300

    Added getMessage method

--- lab02NetBeans/app/src/main/java/lab02NetBeans/App.java
+++ lab02NetBeans/app/src/main/java/lab02NetBeans/App.java
@@ -11,4 +11,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+    public static String getMessage() {
+        return "Hello World";
+    }
 }
