3c1bc5e5b3191f11339a592f7c1ee9cbcb1097ea
Shay Stevens <stesh969@student.otago.ac.nz>
2022-03-11 11:54:25 +1300

    New method
    I've added a new method called 'printMessage'

--- lab02NetBeans/app/src/main/java/lab02NetBeans/App.java
+++ lab02NetBeans/app/src/main/java/lab02NetBeans/App.java
@@ -7,6 +7,10 @@ public class App {
     public String getGreeting() {
         return "Hello World!";
     }
+    
+    public static void printMessage() {
+        System.out.println("Hello World");
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello World");
